4ac0360678b92b401b720edc46725be1f0a41fd2
luuufan <angelia.fan@gmail.com>
2018-01-24 23:51:42 -0800

    user & session generated

--- celp/app/controllers/application_controller.rb
+++ celp/app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user
+
+  def current_user
+    return nil if session[:session_token].nil?
+    current_user = User.find_by(session_token: sessionp[:session_token])
+  end
+
+  def require_current_user
+    if current_user.nil?
+      redirect_to new_session_url
+    end
+  end
 end
---
+++ celp/frontend/entry.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import configureStore from './store/store';
+// import Root from './components/root';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<h1>Celp</h1>, document.getElementById('root'));
+});
