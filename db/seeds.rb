564292c70fd0732e48e77f60573e79250b2831d3
luuufan <angelia.fan@gmail.com>
2018-01-31 09:52:18 -0800

    modified seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.destroy_all
-User.create(username: 'demo_users', password: 'password')
-User.create(username: 'luufan', password:'shasha')
-User.create(username: 'shasha', password: 'shasha')
-User.create(username: 'night', password: 'night123')
+User.create(username: 'demo_users', password: 'password', email:'demo@gmail.com')
+User.create(username: 'luufan', password:'shasha', email:'luuufan@gmail.com')
+User.create(username: 'shasha', password: 'shasha', email:'shasha@gmail.com')
+User.create(username: 'night', password: 'night123', email:'night@gmail.com')
