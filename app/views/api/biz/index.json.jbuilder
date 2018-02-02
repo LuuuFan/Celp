34ac10a94e1637f16b018805b5506d68585b3522
luuufan <angelia.fan@gmail.com>
2018-02-02 00:00:30 -0800

    fixed no img_url index issue

--- app/views/api/biz/index.json.jbuilder
+++ app/views/api/biz/index.json.jbuilder
@@ -7,7 +7,9 @@
 @bizes.each do |biz|
   json.set! biz.id do
     json.extract! biz, :id, :name, :phone, :price
-    json.set! :img_url, biz.imgs.first.url
+    if !biz.imgs.empty?
+      json.set! :img_url, biz.imgs.first.url
+    end
     json.set! :location do
       json.extract! biz, :address1, :address2, :address3, :city, :state, :zip_code
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,60 +1,71 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# # This file should contain all the record creation needed to seed the database with its default values.
+# # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# #
+# # Examples:
+# #
+# #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+# #   Character.create(name: 'Luke', movie: movies.first)
 #
-# Examples:
+# User.destroy_all
+# User.create(username: 'demo_users', password: 'password', email:'demo@gmail.com')
+# User.create(username: 'luufan', password:'shasha', email:'luuufan@gmail.com')
+# User.create(username: 'shasha', password: 'shasha', email:'shasha@gmail.com')
+# User.create(username: 'night', password: 'night123', email:'night@gmail.com')
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Biz.destroy_all
+# Biz.create(
+#   name: 'Sungrass Bakery',
+#   address1: "1730 O'farrell St",
+#   city:'San Francisco',
+#   zip_code:'94115',
+#   state:'CA',
+#   phone:'(415) 000-0000'
+# )
+# Biz.create(
+#   name: 'Shasha Café',
+#   address1: "1730 O'farrell St",
+#   address2: "#888",
+#   city:'San Francisco',
+#   zip_code:'94115',
+#   state:'CA',
+#   phone:'(415) 000-0000'
+# )
+# Biz.create(
+#   name: 'Fat Angel',
+#   address1: "1740 O'farrell St",
+#   address2: "",
+#   city:'San Francisco',
+#   zip_code:'94115',
+#   state:'CA',
+#   phone:'(415) 525-3013'
+# )
 
-User.destroy_all
-User.create(username: 'demo_users', password: 'password', email:'demo@gmail.com')
-User.create(username: 'luufan', password:'shasha', email:'luuufan@gmail.com')
-User.create(username: 'shasha', password: 'shasha', email:'shasha@gmail.com')
-User.create(username: 'night', password: 'night123', email:'night@gmail.com')
-
-Biz.destroy_all
 Biz.create(
-  name: 'Sungrass Bakery',
-  address1: "1730 O'farrell St",
+  name: 'Fog Harbor Fish House',
+  address1: "Pier 39",
+  address2: "Ste A-202",
   city:'San Francisco',
-  zip_code:'94115',
+  zip_code:'94133',
   state:'CA',
-  phone:'(415) 000-0000'
+  phone:'(415) 421-2442',
+  website: 'fogharbor.com'
 )
-Biz.create(
-  name: 'Shasha Café',
-  address1: "1730 O'farrell St",
-  address2: "#888",
-  city:'San Francisco',
-  zip_code:'94115',
-  state:'CA',
-  phone:'(415) 000-0000'
-)
-Biz.create(
-  name: 'Fat Angel',
-  address1: "1740 O'farrell St",
-  address2: "",
-  city:'San Francisco',
-  zip_code:'94115',
-  state:'CA',
-  phone:'(415) 525-3013'
-)
-
-Img.destroy_all
-Img.create(
-  biz_id: 7,
-  user_id: 5,
-  url: 'https://scontent-lax3-1.xx.fbcdn.net/v/t31.0-8/1911276_10202656012097181_1086564781_o.jpg?oh=b78d1e273835e7866c33dbfa6a67b14b&oe=5B250003'
-)
-
-Img.create(
-  biz_id: 7,
-  user_id: 5,
-  url: 'https://scontent-lax3-1.xx.fbcdn.net/v/t31.0-8/1487994_10202453162466067_1183927869_o.jpg?oh=965e3bb005bd86a4858fe85048640aed&oe=5AEAEB97'
-)
-
-Img.create(
-  biz_id: 7,
-  user_id: 5,
-  url: 'https://scontent-lax3-1.xx.fbcdn.net/v/t31.0-8/1501349_10202453168466217_804954821_o.jpg?oh=fcb423a163691b33856b052bb38dfe24&oe=5ADDCFDB'
-)
+#
+# Img.destroy_all
+# Img.create(
+#   biz_id: 7,
+#   user_id: 5,
+#   url: 'https://scontent-lax3-1.xx.fbcdn.net/v/t31.0-8/1911276_10202656012097181_1086564781_o.jpg?oh=b78d1e273835e7866c33dbfa6a67b14b&oe=5B250003'
+# )
+#
+# Img.create(
+#   biz_id: 7,
+#   user_id: 5,
+#   url: 'https://scontent-lax3-1.xx.fbcdn.net/v/t31.0-8/1487994_10202453162466067_1183927869_o.jpg?oh=965e3bb005bd86a4858fe85048640aed&oe=5AEAEB97'
+# )
+#
+# Img.create(
+#   biz_id: 7,
+#   user_id: 5,
+#   url: 'https://scontent-lax3-1.xx.fbcdn.net/v/t31.0-8/1501349_10202453168466217_804954821_o.jpg?oh=fcb423a163691b33856b052bb38dfe24&oe=5ADDCFDB'
+# )
