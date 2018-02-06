ef28468cc0f68c6801dd438ed2be753d20df3d79
luuufan <angelia.fan@gmail.com>
2018-02-06 12:50:04 -0800

    add validation for review rate, have to in 1 - 5

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ApplicationRecord
   validates :body, presence: true
   validates :biz_id, uniqueness: { scope: [:user_id], message: 'You already left a review for this business'}
+  validates :rate, inclusion: {in: [1..5], message:"Please select a rate"}
   belongs_to :biz
   belongs_to :user
 end
