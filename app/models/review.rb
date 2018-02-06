80c5f7a7f7a1eb156bb6fc8baeef7232b988456a
luuufan <angelia.fan@gmail.com>
2018-02-06 12:59:05 -0800

    fixed validation issue

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
   validates :body, presence: true
   validates :biz_id, uniqueness: { scope: [:user_id], message: 'You already left a review for this business'}
-  validates :rate, inclusion: {in: [1..5], message:"Please select a rate"}
+  validates :rate, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
   belongs_to :biz
   belongs_to :user
 end
