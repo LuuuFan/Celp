d694f415d2ae970ce107a8ddb88025fecfc76b9c
luuufan <angelia.fan@gmail.com>
2018-03-15 12:12:30 -0700

    change back to config

--- webpack.config.js
+++ webpack.config.js
@@ -7,10 +7,9 @@ let devPlugins = []; // if using any plugins for development
 const prodPlugins = [
   new webpack.DefinePlugin({
     'process.env': {
-      'NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+      'NODE_ENV': JSON.stringify('production')
     }
   }),
-  new webpack.EnvironmentPlugin(['NODE_ENV']),
   new webpack.optimize.UglifyJsPlugin({
     compress: {
       warnings: true
@@ -19,7 +18,7 @@ const prodPlugins = [
 ];
 
 plugins = plugins.concat(
-  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+  process.env.NODE_ENV == 'production' ? prodPlugins : devPlugins
 )
 
 module.exports = {
