c8dd7bea97d8f7c34f584ffa6ba242d4542be4f1
luuufan <angelia.fan@gmail.com>
2018-03-21 16:40:24 -0700

    trying to fix twilio production issue

--- app/models/twilio_text_messenger.rb
+++ app/models/twilio_text_messenger.rb
@@ -6,7 +6,9 @@ class TwilioTextMessenger
   end
 
   def call(phone_number)
-    client = Twilio::REST::Client.new
+    account_sid = Rails.application.secrets.twilio_account_sid
+    auth_token = Rails.application.secrets.twilio_auth_token
+    client = Twilio::REST::Client.new account_sid, auth_token
     client.messages.create({
       from: Rails.application.secrets.twilio_phone_number,
       to: phone_number,
